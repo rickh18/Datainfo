61f6c52fdef6230c66978fa79afaad4b8178c182
rickh18 <rickharms18@gmail.com>
2014-05-06 14:37:52 +0200

    Exercise 1

---
+++ Session2/exercise1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE problem1(
+	task CHAR(1) PRIMARY KEY,
+	result BOOLEAN,
+	motivation VARCHAR(200)
+);
+
+
---
+++ Session2/result1.sql
@@ -0,0 +1,11 @@
+INSERT INTO problem1 VALUES
+	('a',false,'Een capaciteit kan bij meerder voertuigen horen, capaciteit impliceert geen voertuig.'),
+	('b',true, 'Een voertuig heeft maar een capaciteit'),
+	('c',false,'Een factuur kan meerdere pakketjes omvatten die op hetzelfde adres worden afgeleverd.'),
+	('d',true, 'Een pakketje heeft maar een afleveradres en een factuur.'),
+	('e',true, 'Een pakketje heeft maar een geldbedrag en een ontvanger.'),
+	('f',true, 'Bezorger B kan maar een voertuig besturen op tijdstop t. '),
+	('g',true,'voertuig impliceert capaciteit en een voertuig heeft maar een bestuurder op tijdstip t'),
+	('h',false,''),
+	('i',false,''),
+	('j',false,'');
