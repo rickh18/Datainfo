563d2725038136635aa03f0266e653104833dc5b
rickh18 <rickharms18@gmail.com>
2014-05-06 16:15:39 +0200

    exercise 3c step 4

--- Session2/result3c.sql
+++ Session2/result3c.sql
@@ -1,13 +1,8 @@
-SELECT p.pid FROM Person p
-	WHERE EXISTS(
-		SELECT * FROM Writes w
-		WHERE w.pid = p.pid
-		AND EXISTS(
-		SELECT * FROM Movie m
-		Where m.mid = w.mid
-		AND NOT EXISTS (
-		SELECT * FROM  Directs d
-			WHERE  m.mid = d.mid
-		)
-		)
+SELECT * FROM Person p, Writes w, Movie m
+	WHERE p.pid = w.pid
+	AND  m.mid = w.mid
+	AND NOT EXISTS(
+		SELECT * FROM Directs d
+		WHERE m.mid = d.mid
 	)
+		
