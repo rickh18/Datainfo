1bb2566d5f826be149705054b968751fc429ba40
rickh18 <rickharms18@gmail.com>
2014-05-06 16:15:01 +0200

    exercise 3c step3

--- Session2/result3c.sql
+++ Session2/result3c.sql
@@ -1,6 +1,13 @@
 SELECT p.pid FROM Person p
-	WHERE (
+	WHERE EXISTS(
 		SELECT * FROM Writes w
 		WHERE w.pid = p.pid
-		AND 'movie has no director'
-	);
+		AND EXISTS(
+		SELECT * FROM Movie m
+		Where m.mid = w.mid
+		AND NOT EXISTS (
+		SELECT * FROM  Directs d
+			WHERE  m.mid = d.mid
+		)
+		)
+	)
