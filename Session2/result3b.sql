ac81becc8dc3961677579ac6a8184d90a51c7d7d
rickh18 <rickharms18@gmail.com>
2014-05-06 15:35:32 +0200

    exercise 3b v3

--- Session2/result3b.sql
+++ Session2/result3b.sql
@@ -1,6 +1,5 @@
-SELECT * FROM Movie m, Acts a, Person p
-	WHERE (
-		a.mid = m.mid 
-		AND a.pid = p.pid
-		AND m.name = 'Back to the Future'
+SELECT p.pid FROM Movie m, Acts a, Person p
+	WHERE (a.mid = m.mid 
+	AND a.pid = p.pid
+	AND m.name = 'Back to the Future'
 );
